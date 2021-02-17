e6cdf8a16aa1b2bc33dcbda7634fb681fa063927
Sultansha <nataliiavalko.vnv@gmail.com>
2021-02-17 18:49:21 +0200

    Убирает джс

--- js/menu.js
+++ js/menu.js
@@ -13,9 +13,3 @@
   })
 })();
 
-(() => {
-    const menuBtnRef = document.querySelector('[data-menu-button]');
-    menuBtnRef.addEventListener("click", () => {
-        menuBtnRef.classList.toggle("is-open");     
-  })
-})();
