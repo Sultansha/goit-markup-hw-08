80389beebba8b64ef9fd2b176bd77511e6ba9f10
Sultansha <nataliiavalko.vnv@gmail.com>
2021-03-06 02:05:35 +0200

    Добавляет скрипт на меню

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,18 @@
+
 (() => {
-    const menuBtnRef = document.querySelector('[data-menu-button]');
-   // const mobileMenuRef = document.querySelector('[data-menu]');    
-        
-    menuBtnRef.addEventListener("click", () => {
-       // const expanded =
-       //     menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-        
-        menuBtnRef.classList.toggle("is-open");
-       // menuBtnRef.setAttribute("artia-expanded", !expanded);
-        
-       // mobileMenuRef.classList.toggle('is-open');
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  
+    
+  menuBtnRef.addEventListener("click", () => {
+    menuBtnRef.classList.toggle("is-open");
   })
+
+  menuBtnRef.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    document.body.classList.toggle("menu-open");
+   
+  }    
 })();
+
 
